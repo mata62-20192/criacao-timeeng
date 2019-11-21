e1bd8152f1fa66dc894fbd36383639e0b2ebe348
César Augusto Neves Reis <cesarnevesreis@gmail.com>
2019-11-21 18:16:36 -0300

    Update FactoryMARKDOWN.java

--- src/ufba/AbstractFactory/FactoryMARKDOWN.java
+++ src/ufba/AbstractFactory/FactoryMARKDOWN.java
@@ -1,5 +1,24 @@
 package ufba.AbstractFactory;
+import ufba.elementos.Cabecalho;
+import ufba.elementos.Corpo;
+import ufba.elementos.Rodape;
+import ufba.elementos.markdown.CabecalhoMarkdown;
+import ufba.elementos.markdown.CorpoMarkdown;
+import ufba.elementos.markdown.RodapeMarkdown;
 
-public class FactoryMARKDOWN {
+public class FactoryMARKDOWN extends DocumentacaoFactory{
+    public FactoryMARKDOWN() {
+    }
 
+    public Cabecalho criarCabecalho() {
+        return new CabecalhoMarkdown();
+    }
+
+    public Corpo criarCorpo() {
+        return new CorpoMarkdown();
+    }
+
+    public Rodape criarRodape() {
+        return new RodapeMarkdown();
+    }
 }
