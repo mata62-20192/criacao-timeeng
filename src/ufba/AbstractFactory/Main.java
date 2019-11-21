5c439e257dcf5d894cb516fd0d0b080e806ea2dc
César Augusto Neves Reis <cesarnevesreis@gmail.com>
2019-11-21 18:22:37 -0300

    Update Main.java

--- src/ufba/AbstractFactory/Main.java
+++ src/ufba/AbstractFactory/Main.java
@@ -1,5 +1,31 @@
 package ufba.AbstractFactory;
+import java.util.Date;
+import ufba.elementos.Cabecalho;
+import ufba.elementos.Corpo;
+import ufba.elementos.Rodape;
 
 public class Main {
+    
+    public static void main(String[] Args) {
+        DocumentacaoFactory factoryHTML = new FactoryHTML();
+        CriadorDocumentacao criador = new CriadorDocumentacao(factoryHTML);
+        
+        Cabecalho cabecalho = criador.gerarCabecalho();
+        Corpo corpo = criador.gerarCorpo();
+        Rodape rodape = criador.gerarRodape();
+        System.out.println(cabecalho.getOutput());
+        System.out.println(corpo.getOutput());
+        System.out.println(rodape.getOutput());
+        DocumentacaoFactory factoryMARKDOWN = new FactoryMARKDOWN();
+        criador.setFactory(factoryMARKDOWN);
+        cabecalho = criador.gerarCabecalho();
+        corpo = criador.gerarCorpo();
+        rodape = criador.gerarRodape();
+        System.out.println(cabecalho.getOutput());
+        System.out.println(corpo.getOutput());
+        System.out.println(rodape.getOutput());
+        
+        
+    }
 
 }
