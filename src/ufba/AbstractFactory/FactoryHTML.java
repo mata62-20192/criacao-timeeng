e3492dcdd284ec93c03af4971bba1a73df7ca504
César Augusto Neves Reis <cesarnevesreis@gmail.com>
2019-11-21 18:13:44 -0300

    Update FactoryHTML.java

--- src/ufba/AbstractFactory/FactoryHTML.java
+++ src/ufba/AbstractFactory/FactoryHTML.java
@@ -1,5 +1,24 @@
 package ufba.AbstractFactory;
+import ufba.elementos.Cabecalho;
+import ufba.elementos.Corpo;
+import ufba.elementos.Rodape;
+import ufba.elementos.html.CabecalhoHTML;
+import ufba.elementos.html.CorpoHTML;
+import ufba.elementos.html.RodapeHTML;
 
-public class FactoryHTML {
+public class FactoryHTML extends FactoryDocuentacao {
+    public FactoryHTML() {
+    }
 
+    public Cabecalho criarCabecalho() {
+        return new CabecalhoHTML();
+    }
+
+    public Corpo criarCorpo() {
+        return new CorpoHTML();
+    }
+
+    public Rodape criarRodape() {
+        return new RodapeHTML();
+    }
 }
