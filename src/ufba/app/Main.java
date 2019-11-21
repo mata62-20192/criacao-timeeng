ac96612d06a825259f07ed678dd68ce0ebe1d7e4
CesarGbi <cesarnevesreis@gmail.com>
2019-11-21 17:43:39 -0300

    ...

--- src/ufba/app/Main.java
+++ src/ufba/app/Main.java
@@ -3,7 +3,7 @@ package ufba.app;
 public class Main {
 
 	public static void main(String[] args) {
-		CriadorDocumentacao criador = new CriadorDocumentacao(TipoDocumento.MARKDOWN);
+		CriadorDocumentacao criador = new CriadorDocumentacao(TipoDocumento.HTML);
 		System.out.println(criador.geraDocumentacao());
 	}
 
